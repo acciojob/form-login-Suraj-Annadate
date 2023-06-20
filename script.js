2da4763178c475da38270cc983efa1399fce8be6
Suraj Annadate <124056028+Suraj-Annadate@users.noreply.github.com>
2023-06-20 16:12:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 function getFormvalue() {
     //Write your code here
+ var firstName = document.getElementById("firstName").value;
+  var lastName = document.getElementById("lastName").value;
 
+  alert("First Name: " + firstName + "\nLast Name: " + lastName);
 }
+var form = document.getElementById("myForm");
+form.addEventListener("submit", function(event) {
+  event.preventDefault(); 
+  getFormValue(); // Call the function to retrieve form values
+});
