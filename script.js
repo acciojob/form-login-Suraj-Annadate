fd45371bd62f10a6a678ae17f8c05b35b3b241b2
Suraj Annadate <124056028+Suraj-Annadate@users.noreply.github.com>
2023-07-30 18:53:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,7 @@
 function getFormvalue() {
-  var firstName = document.querySelector('input[name="fname"]').value;
-  var lastName = document.querySelector('input[name="lname"]').value;
-
-  alert(firstName + " "+lastName);
+    //Write your code here
+ const Formele=document.getElementById("form1");
+	const name=Formele.elements['fname'].value;
+	const lname=Formele.elements['lname'].value;
+	alert(`${name} ${lname}`);
 }
-
-// Add event listener to the form submit event
-document.addEventListener("DOMContentLoaded", function() {
-  var form = document.querySelector('#form1');
-  form.addEventListener("submit", function(event) {
-    event.preventDefault(); // Prevent the default form submission
-    getFormValue(); // Call the function to retrieve form values
-  });
-});
